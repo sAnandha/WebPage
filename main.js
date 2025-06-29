39137216b82d02bbe4016726155f58aa85eab853
S.Anandha Nivas <115523613+sAnandha@users.noreply.github.com>
2025-06-30 00:04:58 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-import { UserManager } from "oidc-client-ts";
+// main.js
+import { UserManager } from "https://cdn.jsdelivr.net/npm/oidc-client-ts/+esm";
 
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_E1V33d9fq",
-    client_id: "2g17vttj8eeb7fhacugmmfnkeq",
-    redirect_uri: "https://main.de43hd7rcr7qe.amplifyapp.com/dashboard.html",
-    response_type: "code",
-    scope: "email openid phone"
+  authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_E1V33d9fq", // Replace with your user pool
+  client_id: "2g17vttj8eeb7fhacugmmfnkeq",
+  redirect_uri: "https://main.de43hd7rcr7qe.amplifyapp.com/dashboard.html",
+  response_type: "code",
+  scope: "openid email profile"
 };
 
 export const userManager = new UserManager({
-    ...cognitoAuthConfig,
+  ...cognitoAuthConfig
 });
 
-export async function signOutRedirect () {
-    const logoutUri = "https://main.de43hd7rcr7qe.amplifyapp.com/";
-    const cognitoDomain = "https://us-east-1e1v33d9fq.auth.us-east-1.amazoncognito.com";
-    window.location.href = `${cognitoDomain}/logout?client_id=2g17vttj8eeb7fhacugmmfnkeq&logout_uri=${encodeURIComponent(logoutUri)}`;
+export async function signOutRedirect() {
+  const clientId = "2g17vttj8eeb7fhacugmmfnkeq";
+  const logoutUri = "https://main.de43hd7rcr7qe.amplifyapp.com";
+  const cognitoDomain = "https://us-east-1e1v33d9fq.auth.us-east-1.amazoncognito.com";
+  window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 }
